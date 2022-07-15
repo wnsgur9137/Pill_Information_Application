d7936224d1a2feedc135b8d9b4fa741632f83b44
Junhyeok <83490421+wnsgur9137@users.noreply.github.com>
2022-07-15 20:40:34 +0900

    07_15 API test

--- Pill_Information/AppDelegate.swift
+++ Pill_Information/AppDelegate.swift
@@ -9,51 +9,117 @@ import UIKit
 import FirebaseCore
 import FirebaseAuth
 import FirebaseFirestore
+import FirebaseDatabase
 import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     
-//    struct hospitalData: Decodable{
-//        let currentCount: Int
-//        let data: [Data]
-//    }
-//
-//    struct Data: Decodable{
-//        var pageNo: String
-//        var numOfRows: String
-//        var entpName: String // 업체명
-//        var itemName: String //제품명
-//        var itemSeq: String //품목기준코드
-//        var efcyQesitm: String // 효능
-//        var useMethodQesitm:String // 사용법
-//        var atpnWarnQesitm: String // 주의사항, 경고
-//        var atpnQesitm: String // 주의사항
-//        var intrcQesitm: String // 상호작용
-//        var seQesitm: String // 부작용
-//        var depositMethodQesitm: String // 보관법
-//        var openDe: String // 공개일자
-//        var updateDe: String // 수정일자
-//        var itemImage: String // 낱알 이미지
-//    }
-//    
+    var ref: DatabaseReference!
+    
+    // MARK: - 낱알 정보 API
+    
+    struct pillData: Decodable{
+        let header: HeaderData
+        let body: BodyData
+    }
+    
+    struct HeaderData: Decodable {
+        let resultCode: String
+        let resultMsg: String
+    }
+    
+    struct BodyData: Decodable {
+        let pageNo: Int
+        let totalCount: Int
+        let numOfRows: Int
+        let items: [Data]
+    }
+
+    struct Data: Decodable{
+        let resultCode: String? // 결과 코드
+        let resultMsg: String? // 결과 메세지
+        let numOfRows: String? // 한 페이지 결과 수
+        let pageNo: String? // 페이지 번호
+        let totalCount: String? // 전체 결과 수
+        let entpName: String? // 업체명
+        
+        let itemName: String? //제품명
+        let itemSeq: String? //품목기준코드
+        
+        let efcyQesitm: String? // 효능
+        let useMethodQesitm:String? // 사용법
+        let atpnWarnQesitm: String? // 주의사항, 경고
+        let atpnQesitm: String? // 주의사항
+        let intrcQesitm: String? // 상호작용
+        let seQesitm: String? // 부작용
+        let depositMethodQesitm: String? // 보관법
+        let openDe: String? // 공개일자
+        let updateDe: String? // 수정일자
+        let itemImage: String? // 낱알 이미지
+    }
+    
+    
+    func getDrbEasyDrugList() {
+        for i in 1...45 {
+            if let url = URL(string:"http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?serviceKey=LK5HxMQO7ScyFpgYc6%2BQgNRsqPfAKmnW1fczw8kHYE4BDXCaUX7uBUrZXK6wXoDnS5vivk2h0fYiboTWVjRjvQ%3D%3D&pageNo=1&startPage=\(i)&numOfRows=100&type=json") {
+                let request = URLRequest.init(url: url)
+                
+                URLSession.shared.dataTask(with: request) {
+                    (data, response, error) in guard let data = data else {return}
+                    let decoder = JSONDecoder()
+                    do{
+                        let json = try decoder.decode(pillData.self , from: data)
+                        print("json:")
+                        print(json)
+                        self.saveJsonDatabase(json: json)
+                    }
+                    catch{
+                        print("API 에러")
+                        print(error)
+                    }
+                }.resume()
+            }
+        }
+    }
+    
+    func saveJsonDatabase(json: AppDelegate.pillData) {
+        let data = json.body.items
+        print("\n\n\nitemName:")
+        print(data[0])
+        
+//        Database.database().reference().child("Data").child("").setValue(
+//            ["itemName":"123"])
+    }
+
+    
+    // MARK: - 의약품 정보 API
+    
+    
 //    func getDrbEasyDrugList() {
-//        if let url = URL(string:"http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?serviceKey=서비스키(URLEncode)&trustEntpName=한미약품(주)&pageNo=1&startPage=1&numOfRows=3") {
+//        if let url = URL(string:"http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?serviceKey=LK5HxMQO7ScyFpgYc6%2BQgNRsqPfAKmnW1fczw8kHYE4BDXCaUX7uBUrZXK6wXoDnS5vivk2h0fYiboTWVjRjvQ%3D%3D&pageNo=1&startPage=1&numOfRows=3&type=json") {
 //            let request = URLRequest.init(url: url)
 //
+//            print("url:")
 //            URLSession.shared.dataTask(with: request) {
 //                (data, response, error) in guard let data = data else {return}
 //                let decoder = JSONDecoder()
 //                print(response as Any)
-//                do{ let json = try decoder.decode(hospitalData.self , from: data)
+//                do{
+//                    let json = try decoder.decode(pillData.self , from: data)
+//                    print("json:")
 //                     print(json)
+////                    self.returnData = String(json!)
 //                }
 //                catch{
+//                    print("error")
 //                    print(error)
+////                    self.returnError = String(error!)
 //                }
 //            }.resume()
 //        }
 //    }
+//
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -62,6 +128,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
         GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
         GIDSignIn.sharedInstance().delegate = self
         
+        print("----------------\n\n\n\n\n")
+        getDrbEasyDrugList()
+        print("\n\n\n\n\n----------------")
         
 //        getDrbEasyDrugList()
         return true
@@ -90,8 +159,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     
     // 메인 화면으로 이동하기
     private func showMainViewController() {
-        let storyboard = UIStoryboard(name: "mainBoard", bundle: Bundle.main)
-        let mainViewController = storyboard.instantiateViewController(withIdentifier: "mainBoard")
+        let storyboard = UIStoryboard(name: "MainBoard", bundle: Bundle.main)
+        let mainViewController = storyboard.instantiateViewController(withIdentifier: "MainBoard")
         mainViewController.modalPresentationStyle = .fullScreen
         UIApplication.shared.windows.first?.rootViewController?.show(mainViewController, sender: nil)
     }
